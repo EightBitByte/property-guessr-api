3c29f21f85e7438a213b7e04a68d0902ad1e26ae
Leah Sun <leahsun@dhcp-v093-052.mobile.uci.edu>
2024-01-27 09:21:57 -0800

    finished addresses.py

--- src/addresses.py
+++ src/addresses.py
@@ -1,2 +1,29 @@
-from random_address import real_random_address
+#from random_address import real_random_address
+import random_address
+#random_address.real_random_address_by_state('CA')
+#we must get random state!!
 
+import random
+
+#f”{address1}, {city}, {state}, {postalCode}
+#Ex. “1030 North Princeton Avenue, Fullerton, CA, 92831”
+
+
+def lat_lon_freeform() -> (float, float, str):
+    addy = random_address.real_random_address_by_state("CA")
+    print(addy)
+    address1 = addy['address1'] + ","
+    city = addy['city'] + ", CA,"
+    postal = addy['postalCode']
+    print(addy['coordinates']['lat'])
+    freeform = "{in1} {in2} {in3}".format(in1=address1, in2=city, in3=postal)
+    print('String Concatenation using format() =', freeform)
+    print(addy['coordinates']['lat'], addy['coordinates']['lng'], freeform)
+    return (addy['coordinates']['lat'], addy['coordinates']['lng'], freeform)    
+#lat/lon ??
+def main():
+    lat_lon_freeform()
+
+
+if __name__ == "__main__":
+    main()
