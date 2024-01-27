505f185738843528e948e02cbf971e835ee64711
Peter Tumali <ptumali@uci.edu>
2024-01-27 12:41:26 -0800

    Random address generator (documented) and restructured

--- src/addresses.py
+++ src/addresses.py
@@ -1,28 +1,44 @@
-#from random_address import real_random_address
-import random_address
-#random_address.real_random_address_by_state('CA')
-#we must get random state!!
-
+from random_address import real_random_address_by_state
 import random
+from collections import namedtuple
 
-#f”{address1}, {city}, {state}, {postalCode}
-#Ex. “1030 North Princeton Avenue, Fullerton, CA, 92831”
+Coord = namedtuple("Coord", ['lat', 'long'])
 
+class RandomAddress:
+    """
+    A class that serves to generate a random address
+    """
+    def __init__(self):
+        self._state = "CA"
+        self._address = self._generate_random_address()
+    
+    def _generate_random_address(self) -> dict:
+        """
+        Returns a random address in the form of a dictionary using the random_address library
+        """
+        address_dict = real_random_address_by_state("CA")
+        return address_dict
 
-def lat_lon_freeform() -> (float, float, str):
-    addy = random_address.real_random_address_by_state("CA")
-    print(addy)
-    address1 = addy['address1'] + ","
-    city = addy['city'] + ", CA,"
-    postal = addy['postalCode']
-    print(addy['coordinates']['lat'])
-    freeform = "{in1} {in2} {in3}".format(in1=address1, in2=city, in3=postal)
-    print('String Concatenation using format() =', freeform)
-    print(addy['coordinates']['lat'], addy['coordinates']['lng'], freeform)
-    return (addy['coordinates']['lat'], addy['coordinates']['lng'], freeform)    
-#lat/lon ??
+    def format_address(self) -> str:
+        """
+        Returns a formated string of the random address
+        """
+        address_dict = self._address
+        return f"{address_dict['address1']}, {address_dict['city']}, {address_dict['state']}, {address_dict['postalCode']}"
+
+    def generate_coord(self) -> Coord(float, float):
+        """
+        Returns a namedtuple of the coordinates (latitude, longitude) of the random address
+        """
+        address_dict = self._address
+        coordinates = Coord(address_dict['coordinates']['lat'], address_dict['coordinates']['lng'])
+        return coordinates
+
 def main():
-    lat_lon_freeform()
+    some_address = RandomAddress()
+    print(some_address.format_address())
+    print(some_address.generate_coord())
+    
 
 
 if __name__ == "__main__":
